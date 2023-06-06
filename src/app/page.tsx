4c761a1b269305807282a83f0f5d58c4c88473ff
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-06-06 14:04:53 -0400

    Added components to page and typed savingsInfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
+'use client'
+
 import CalculatorForm from "@/components/CalculatorForm"
+import FAQ from "@/components/FAQ"
+import SavingsInfo from "@/components/SavingsInfo"
+import { useState, useEffect } from "react"
+
+interface SavingsInfo {
+  id: number,
+  item: string, 
+  description: string,
+  amount?: number, 
+  createdAt: string,
+  updatedAt: string,
+  main_savings?: {
+    id: number,
+    item: string,
+    description: string,
+    amount: number,
+    createdAt: string,
+    updatedAt: string,
+  }[],
+}
 
 export default function Home() {
+  const [savingsInfo, setSavingsInfo] = useState<SavingsInfo>({
+    id: 0,
+    item: "",
+    description: '',
+    createdAt: "",
+    updatedAt: ""
+  })
+
+  const getInitialSavings = async () => {
+
+  }
+
+  useEffect(() => {
+    getInitialSavings()
+  }, [])
+
   return (
     <main>
-      <CalculatorForm />
+      <h2>School Savings Calculator</h2>
+      <h1>How much money can your school save with the Inflation Reduction Act?</h1>
+      <CalculatorForm savingsInfo={savingsInfo} setSavingsInfo={setSavingsInfo}/>
+      <FAQ />
+      <h2>School Electrification Incentives</h2>
+      <p>All the savings your school may be eligible for!</p>
+      <SavingsInfo />
     </main>
   )
 }
