15f101086c073e4cf33c50f2f00c10715d00ba67
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-06-07 19:56:31 -0400

    Set up template for credit and bonuses

--- components/SavingsRow.tsx
+++ components/SavingsRow.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'
+import { useState } from "react"
 
 type SavingsRow = {
     savings: {id: number,
@@ -20,12 +20,29 @@ type SavingsRow = {
 }
 
 const SavingsRow = ({savings}: SavingsRow) => {
+    const [clicked, toggleClicked] = useState(false)
+
+    const handleChange = () => {
+        toggleClicked(!clicked)
+    }
 
     return (
         <div>
             <p>{savings.item}</p>
             <p>{savings.amount ? savings.amount + '%' : 'Varies'}</p>
-            <Link href={`/${savings.id}`}>More Info</Link>
+            <button onClick={handleChange}>More Info</button>
+            {clicked && <div>
+                <h1>{savings.item}</h1>
+                <h1>{savings.amount ? savings.amount + '%' : 'Varies'}</h1>
+                <p>{savings.description}</p>
+                {savings.main_savings?.map((bonus) => (
+                    <div key={bonus.id}>
+                        <h1>{bonus.item}</h1>
+                        <h1>{bonus.amount}</h1>
+                        <p>{bonus.description}</p>
+                    </div>
+                ))}
+            </div>}
         </div>
     )
 }
