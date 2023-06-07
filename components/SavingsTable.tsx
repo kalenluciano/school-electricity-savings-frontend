8fb8e5ed2054959783f7d05132c80e4b30cbe7d9
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-06-07 19:56:11 -0400

    Added headers for table

--- components/SavingsTable.tsx
+++ components/SavingsTable.tsx
@@ -41,7 +41,15 @@ type Savings = {
 const SavingsTable: React.FC<SavingsInfoProps> = ({savingsInfo, calculatedSavings, calculated}) => {
     
     return (
-        calculated ? <div>{calculatedSavings.savings.map((savings: Savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div> : <div>{savingsInfo.map((savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div> 
+        <div>
+            <div>
+                <h2>Item</h2>
+                <h2>Amount</h2>
+            </div>
+            
+            { calculated ? <div>{calculatedSavings.savings.map((savings: Savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div> : <div>{savingsInfo.map((savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div>}
+        </div>
+        
     )
 }
 
