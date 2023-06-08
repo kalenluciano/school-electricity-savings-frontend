099ee16a90cda035fc324ed208efaff6b1ec6402
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-06-08 17:25:46 -0400

    Added classes for styling to SavingsTable component

--- components/SavingsTable.tsx
+++ components/SavingsTable.tsx
@@ -41,13 +41,22 @@ type Savings = {
 const SavingsTable: React.FC<SavingsInfoProps> = ({savingsInfo, calculatedSavings, calculated}) => {
     
     return (
-        <div>
+        <div className='savings-table-section'>
+            <div className='savings-header'>
+                <h2>School Electrification Incentives</h2>
+                <p>All the savings your school may be eligible for!</p>
+            </div>
+
             <div>
-                <h2>Item</h2>
-                <h2>Amount</h2>
+                <div className='savings-table-headers'>
+                    <h3 className='savings-item'>Item</h3>
+                    <h3>Amount</h3>
+                    <div className='more-info'></div>
+                </div>
+                
+                { calculated ? <div>{calculatedSavings.savings.map((savings: Savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div> : <div>{savingsInfo.map((savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div>}
             </div>
             
-            { calculated ? <div>{calculatedSavings.savings.map((savings: Savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div> : <div>{savingsInfo.map((savings) => <div key={savings.id}><SavingsRow savings={savings}/></div>)}</div>}
         </div>
         
     )
