066a3c6f5776d28829b287d5216740cef454012f
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-23 18:27:39 -0400

    Commented out old form assets

--- components/CalculatorForm.tsx
+++ components/CalculatorForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import axios from 'axios'
 import {BASE_URL} from '../src/globals'
 
@@ -10,31 +10,39 @@ type CalculatorFormProps = {
     toggleCalculating: React.Dispatch<React.SetStateAction<any>>
 }
 
-interface FormValues {
-    streetAddress: string,
-    city: string,
-    state: string,
-    zipCode: string
-}
+// interface FormValues {
+//     streetAddress: string,
+//     city: string,
+//     state: string,
+//     zipCode: string
+// }
 
 const CalculatorForm = ({ setCalculatedSavings, toggleCalculated, toggleCalculating } : CalculatorFormProps) => {
-    const initialFormValues = {
-        streetAddress: "",
-        city: "",
-        state: "",
-        zipCode: ""
+    const autoCompleteRef = useRef("")
+    const inputRef = useRef("")
+    const options = {
+        componentRestrictions: { country: "us" },
+        fields: ['formatted address', 'geometry', 'name']
     }
+    
+    // const initialFormValues = {
+    //     streetAddress: "",
+    //     city: "",
+    //     state: "",
+    //     zipCode: ""
+    // }
 
-    const [formValues, setFormValues] = useState<FormValues>(initialFormValues)
+    // const [formValues, setFormValues] = useState<FormValues>(initialFormValues)
 
-    const handleChange = (e: any) => {
-        setFormValues({...formValues, [e.target.name]: e.target.value.replaceAll(" ", "+")})
-    }
+    // const handleChange = (e: any) => {
+    //     setFormValues({...formValues, [e.target.name]: e.target.value.replaceAll(" ", "+")})
+    // }
 
     const handleSubmit = async (e: any) => {
         e.preventDefault()
         toggleCalculating(true)
-        const result = await axios.get(`${BASE_URL}/address/${formValues.streetAddress}/${formValues.city}/${formValues.state}/${formValues.zipCode}`)
+        const result = {data: []}
+        // const result = await axios.get(`${BASE_URL}/address/${formValues.streetAddress}/${formValues.city}/${formValues.state}/${formValues.zipCode}`)
         setCalculatedSavings(result.data)
         toggleCalculating(false)
         toggleCalculated(Object.keys(result.data).length > 0)
@@ -43,7 +51,7 @@ const CalculatorForm = ({ setCalculatedSavings, toggleCalculated, toggleCalculat
     return (
         <div className="calculator-form">
         <h2>Enter your school&apos;s information</h2>
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
             <label htmlFor="streetAddress">Street Address</label>
             <input onChange={handleChange} name="streetAddress" placeholder="633 Clark St" required/>
             <label htmlFor="city">City</label>
@@ -53,7 +61,7 @@ const CalculatorForm = ({ setCalculatedSavings, toggleCalculated, toggleCalculat
             <label htmlFor="zipCode">Zip Code</label>
             <input onChange={handleChange} name="zipCode" placeholder="60208" required />
             <button className="search-button">Search</button>
-        </form>
+        </form> */}
         </div>
     )
 }
