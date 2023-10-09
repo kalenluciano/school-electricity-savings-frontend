0238a1d8fc18fed6fae68e37f2aae5284f1890e6
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-09 17:23:35 -0400

    Add Google Autocomplete to calculator

--- components/CalculatorForm.jsx
+++ components/CalculatorForm.jsx
@@ -3,25 +3,29 @@
 import React, { useState } from "react"
 import axios from 'axios'
 import {BASE_URL} from '../globals'
+import Autocomplete from "react-google-autocomplete";
 
 const CalculatorForm = ({ setCalculatedSavings, toggleCalculated, toggleCalculating }) => {
-    const initialFormValues = {
-        streetAddress: "",
-        city: "",
-        state: "",
-        zipCode: ""
+    const [address, setAddress] = useState("");
+    const [coordinatesLat, setCoordinatesLat] = useState("")
+    const [coordinatesLng, setCoordinatesLng] = useState("")
+
+    const handlePlaceSelected = async (place) => {
+        setAddress(place.formatted_address.replaceAll(' ', '%20').replaceAll(',', '%2C'));
+        if (place.geometry.location) {
+            setCoordinatesLat(place.geometry.location.lat)
+            setCoordinatesLng(place.geometry.location.lng)
+        }
     }
 
-    const [formValues, setFormValues] = useState(initialFormValues)
-
-    const handleChange = (e) => {
-        setFormValues({...formValues, [e.target.name]: e.target.value.replaceAll(" ", "+")})
+    const handleInputChange = (e) => {
+        setAddress(e.target.value.replaceAll(' ', '%20'));
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         toggleCalculating(true)
-        const result = await axios.get(`${BASE_URL}/address/${formValues.streetAddress}/${formValues.city}/${formValues.state}/${formValues.zipCode}`)
+        const result = await axios.get(`${BASE_URL}/address/${address}/${coordinatesLat}/${coordinatesLng}`)
         setCalculatedSavings(result.data)
         toggleCalculating(false)
         toggleCalculated(Object.keys(result.data).length > 0)
@@ -31,14 +35,16 @@ const CalculatorForm = ({ setCalculatedSavings, toggleCalculated, toggleCalculat
         <div className="calculator-form">
         <h2>Enter your school&apos;s information</h2>
         <form onSubmit={handleSubmit}>
-            <label htmlFor="streetAddress">Street Address</label>
-            <input onChange={handleChange} name="streetAddress" placeholder="633 Clark St" required/>
-            <label htmlFor="city">City</label>
-            <input onChange={handleChange} name="city" placeholder="Evanston" required />
-            <label htmlFor="state">State</label>
-            <input onChange={handleChange} name="state" placeholder="IL" required />
-            <label htmlFor="zipCode">Zip Code</label>
-            <input onChange={handleChange} name="zipCode" placeholder="60208" required />
+            <Autocomplete
+                apiKey={process.env.NEXT_PUBLIC_API_KEY_GOOGLE}
+                onPlaceSelected={handlePlaceSelected}
+                onChange={handleInputChange}
+                placeholder="Enter your school's address"
+                options={{
+                    types: ['address'],
+                    fields: ['formatted_address', 'geometry']
+                }}
+            />
             <button className="search-button">Search</button>
         </form>
         </div>
