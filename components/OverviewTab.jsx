0c2e2af7d900364e49e95a20471eb8dc0a99d2db
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-09 23:47:41 -0400

    Set up Overview tab to render data

--- components/OverviewTab.jsx
+++ components/OverviewTab.jsx
@@ -1,6 +1,31 @@
-const OverviewTab = ({savings, calculatedSavings}) => {
+import Image from "next/image"
+
+const OverviewTab = ({overview, calculatedSavings}) => {
   return (
-    <div>OverviewTab</div>
+    <div>
+      <p>{overview.intro}</p>
+      <div>
+        <h3>Average Life Span</h3>
+        <p>{overview.life_span}</p>
+      </div>
+      
+      <div>
+        <h3>Estimated Tax Credits</h3>
+        <p>{overview.tax_credits}</p>
+      </div>
+
+      <div>
+        <h3>Additional Grants</h3>
+        <p>{overview.grants}</p>
+      </div>
+
+      {/* <Image src={overview.image} alt="" width={100} height={100}/> */}
+
+      <div>
+        <h3>How does it work?</h3>
+        <p>{overview.explanation}</p>
+      </div>
+    </div>
   )
 }
 
