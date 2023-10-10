da43e389768554fc10fe12a59b792379d931a9a4
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-09 23:40:18 -0400

    Set up Data Sources tab to render data

--- components/DataSourcesTab.jsx
+++ components/DataSourcesTab.jsx
@@ -1,6 +1,13 @@
-const DataSourcesTab = ({savings, calculatedSavings}) => {
+const DataSourcesTab = ({dataSources, calculatedSavings}) => {
   return (
-    <div>DataSourcesTab</div>
+    <div>
+      {dataSources.map((source, index) => (
+        <div key={index}>
+          <h2>{source.title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: source.content }} />
+        </div>
+      ))}
+    </div>
   )
 }
 
