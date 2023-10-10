c997891cd642e541b84e6e89332c1b9a2f4e7dd7
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-09 23:40:44 -0400

    Set up Benefits tab to render data

--- components/BenefitsTab.jsx
+++ components/BenefitsTab.jsx
@@ -1,6 +1,15 @@
-const BenefitsTab = ({savings, calculatedSavings}) => {
+const BenefitsTab = ({benefits, calculatedSavings}) => {
   return (
-    <div>BenefitsTab</div>
+    <div>
+      <h2>Financial Benefits</h2>
+      <p>{benefits.financial}</p>
+
+      <h2>Environmental Benefits</h2>
+      <p>{benefits.environmental}</p>
+
+      <h2>Health and Other Benefits</h2>
+      <p>{benefits.health_other}</p>
+    </div>
   )
 }
 
