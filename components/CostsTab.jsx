7e2faba10a7e4e21a510db01ab79e79374e37d7f
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-09 23:40:58 -0400

    Set up Costs tab to render data

--- components/CostsTab.jsx
+++ components/CostsTab.jsx
@@ -1,7 +1,32 @@
-const CostsTab = ({savings, calculatedSavings}) => {
+const CostsTab = ({costs, calculatedSavings}) => {
   return (
-    <div>CostsTab</div>
-  )
+    <div>
+
+      <div>
+        <div>
+        <h2>Average Costs</h2>
+        {costs.value.map((value, index) => (
+          <div key={index}>
+            <p>{value}</p>
+          </div>
+        ))}
+        <p>{costs.reference_point}</p>
+        </div>
+      </div>
+
+      <div>
+        <p>{costs.explanation}</p>
+      </div>
+
+      <div>
+        {costs.footers.map((footer, index) => (
+          <div key={index}>
+            <p>{footer}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CostsTab
