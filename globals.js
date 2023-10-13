bac29acb6af53c59b9e1142748d4a0d1072fccab
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-12 23:36:09 -0400

    Set BASE_URL for prod

--- globals.js
+++ globals.js
@@ -1,2 +1,3 @@
-// export const BASE_URL ='https://school-electricity-savings-be.herokuapp.com/api';
-export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL =
+	'https://school-electricity-savings-be.herokuapp.com/api';
+// export const BASE_URL = 'http://localhost:3001/api';
