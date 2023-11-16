f5ddf0a731468734d97446e7c059a1afa1c59d8f
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-11-16 16:43:03 -0500

    Add new images to overview tab

--- components/OverviewTab.jsx
+++ components/OverviewTab.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
-import {AiOutlineQuestionCircle} from 'react-icons/ai'
+import { AiOutlineQuestionCircle } from 'react-icons/ai'
 
-const OverviewTab = ({overview, calculatedSavings, relevantStats}) => {
+const OverviewTab = ({ overview, calculatedSavings, relevantStats, savingsImageSrc }) => {
+
   return (
     <div className="mt-6">
       <div className="flex items-start self-stretch gap-x-4">
@@ -13,7 +14,7 @@ const OverviewTab = ({overview, calculatedSavings, relevantStats}) => {
               <h3 className="text-ink-black font-normal text-base">Average Life Span</h3>
               <p className="text-xl font-semibold text-ink-black">{overview.life_span}</p>
             </div>
-            
+
             <div>
               <div className="flex items-center gap-x-1 relative">
                 <h3 className="text-ink-black font-normal text-base">Estimated Tax Credits</h3>
@@ -21,9 +22,9 @@ const OverviewTab = ({overview, calculatedSavings, relevantStats}) => {
                   <div className="group-hover:block hidden absolute bottom-10 start-36 flex flex-col items-center w-80 py-0.5 px-2 bg-light-blue rounded"><p className="text-sm text-ink-black">We estimated this by mapping your school&apos;s location to census data and IRA&apos;s eligibility requirements.</p><Image src="/assets/triangle.png" alt="A triangle pointing down" width={16} height={8} className="absolute top-[3.875rem] start-4" /></div>
                   <AiOutlineQuestionCircle className="text-ink-black w-3 h-3" />
                 </div>
-                
+
               </div>
-              <p className="text-xl font-semibold text-ink-black">{relevantStats?.total_tax_credit !== undefined ? relevantStats.total_tax_credit + "%" : overview.tax_credits  }</p>
+              <p className="text-xl font-semibold text-ink-black">{relevantStats?.total_tax_credit !== undefined ? relevantStats.total_tax_credit + "%" : overview.tax_credits}</p>
               <p className="text-sm text-dark-gray font-normal">of equipment and installation cost</p>
             </div>
 
@@ -41,7 +42,7 @@ const OverviewTab = ({overview, calculatedSavings, relevantStats}) => {
           </div>
         </div>
 
-        <Image src='/assets/header_image.png' alt="" width={280} height={170} className="w-72 h-44 object-contain object-center"/>
+        <Image src={savingsImageSrc} alt="" width={280} height={170} className="w-72 h-44 object-contain object-center" />
       </div>
 
       <div className="mt-10 flex items-start flex-col self-stretch rounded-lg bg-light-blue p-4 gap-x-2">
--- components/SavingsRow.jsx
+++ components/SavingsRow.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Button } from '@chakra-ui/react'
-import {BsChevronDown, BsChevronUp} from 'react-icons/bs'
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs'
 import OverviewTab from "./OverviewTab"
 import IncentivesTab from "./IncentivesTab"
 import CostsTab from "./CostsTab"
@@ -10,27 +10,34 @@ import BenefitsTab from "./BenefitsTab"
 import DataSourcesTab from "./DataSourcesTab"
 import MoreInfoTabBar from "./MoreInfoTabBar"
 
-const SavingsRow = ({savings, calculatedSavings, calculating}) => {
+const SavingsRow = ({ savings, calculatedSavings, calculating }) => {
     let relevantStats = null
-    
+    let savingsImageSrc = null
+
     switch (savings.item) {
         case "Solar Panel Installation":
             relevantStats = calculatedSavings?.SolarGeoBatteryStat ?? null
+            savingsImageSrc = '/assets/Solar_Panels.jpg'
             break;
         case "Battery Storage Installation":
             relevantStats = calculatedSavings?.SolarGeoBatteryStat ?? null
+            savingsImageSrc = '/assets/Battery_Storage.jpg'
             break;
         case "Electric Vehicles":
             relevantStats = calculatedSavings?.ElectricVehicleStat ?? null
+            savingsImageSrc = '/assets/Electric_Vehicles.jpg'
             break;
         case "Electric Vehicle Charger Installation":
             relevantStats = calculatedSavings?.EVChargerStat ?? null
+            savingsImageSrc = '/assets/Electric_Vehicle_Charger.jpg'
             break;
         case "Building Energy Efficiency":
             relevantStats = null
+            savingsImageSrc = '/assets/Building_Efficiency.jpg'
             break;
         case "Geothermal Heating Installation":
             relevantStats = calculatedSavings?.SolarGeoBatteryStat ?? null
+            savingsImageSrc = '/assets/Geothermal_Heat_Pump.jpg'
             break;
         default:
             relevantStats = null
@@ -49,15 +56,15 @@ const SavingsRow = ({savings, calculatedSavings, calculating}) => {
             <div className={`flex items-center justify-space-between flex-wrap py-3 px-10 ${clicked && "bg-gray-outline"}`}>
                 <p className="w-3/5 font-normal text-base text-ink-black">{savings.item}</p>
                 <p className="w-1/4 font-semibold text-base text-ink-black">{relevantStats?.total_tax_credit !== undefined ? relevantStats?.total_tax_credit + "%" : savings.amount}</p>
-                <Button className="bg-gray-blue text-dark-gray" onClick={handleChange} size={"sm"} colorScheme='gray' variant={"outline"}  rightIcon={!clicked ? <BsChevronDown/> : <BsChevronUp/>}>More info</Button>
+                <Button className="bg-gray-blue text-dark-gray" onClick={handleChange} size={"sm"} colorScheme='gray' variant={"outline"} rightIcon={!clicked ? <BsChevronDown /> : <BsChevronUp />}>More info</Button>
             </div>
-            
+
             {clicked && <div className="w-full px-8 pt-4 pb-8">
                 <MoreInfoTabBar currentTab={currentTab} setCurrentTab={setCurrentTab} />
-                {currentTab === "overview" && <OverviewTab overview={savings.overview} calculatedSavings={calculatedSavings} relevantStats={relevantStats} />}
-                {currentTab === "incentives" &&<IncentivesTab taxIncentives={savings.tax_incentives} additionalGrants={savings.additional_grants} calculatedSavings={calculatedSavings} relevantStats={relevantStats} calculating={calculating} />}
+                {currentTab === "overview" && <OverviewTab overview={savings.overview} calculatedSavings={calculatedSavings} relevantStats={relevantStats} savingsImageSrc={savingsImageSrc} />}
+                {currentTab === "incentives" && <IncentivesTab taxIncentives={savings.tax_incentives} additionalGrants={savings.additional_grants} calculatedSavings={calculatedSavings} relevantStats={relevantStats} calculating={calculating} />}
                 {currentTab === "costs" && <CostsTab costs={savings.costs} calculatedSavings={calculatedSavings} />}
-                {currentTab === "benefits" && <BenefitsTab benefits={savings.benefits} calculatedSavings={calculatedSavings}/>}
+                {currentTab === "benefits" && <BenefitsTab benefits={savings.benefits} calculatedSavings={calculatedSavings} />}
                 {currentTab === "dataSources" && <DataSourcesTab dataSources={savings.data_sources} calculatedSavings={calculatedSavings} />}
             </div>}
         </div>
