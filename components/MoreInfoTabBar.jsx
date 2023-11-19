849419ca40d223c60547b867e67408bfae0fa30e
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-11-19 14:35:23 -0500

    Fix savings row tabs to be responsive

--- components/MoreInfoTabBar.jsx
+++ components/MoreInfoTabBar.jsx
@@ -1,10 +1,10 @@
-const MoreInfoTabBar = ({currentTab, setCurrentTab}) => {
+const MoreInfoTabBar = ({ currentTab, setCurrentTab }) => {
   const handleClick = (e) => {
     setCurrentTab(e.target.getAttribute('data'))
   }
 
   return (
-    <div className="flex items-start self-stretch border-b-2 border-gray-blue">
+    <div className="flex items-start self-stretch border-b-2 border-gray-blue overflow-scroll whitespace-nowrap">
       <p data="overview" onClick={handleClick} className={`cursor-pointer ps-4 pe-8 py-1 text-sm font-normal text-dark-gray ${currentTab === "overview" && "font-semibold text-deep-blue border-b-2 border-deep-blue"}`}>Overview</p>
       <p data="incentives" onClick={handleClick} className={`cursor-pointer ps-4 pe-8 py-1 text-sm font-normal text-dark-gray ${currentTab === "incentives" && "font-semibold text-deep-blue border-b-2 border-deep-blue"}`}>Incentives</p>
       <p data="costs" onClick={handleClick} className={`cursor-pointer ps-4 pe-8 py-1 text-sm font-normal text-dark-gray ${currentTab === "costs" && "font-semibold text-deep-blue border-b-2 border-deep-blue"}`}>Costs</p>
