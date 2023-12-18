39aee9d3813e9c7ce65d939764fca5665e5e2a37
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-12-18 16:58:07 -0500

    Reformat page file structure

---
+++ app/home/page.js
@@ -0,0 +1,36 @@
+'use client';
+
+import Header from '@/components/Header';
+import CalculatorForm from '@/components/CalculatorForm';
+import FAQ from '@/components/FAQ';
+import SavingsTable from '@/components/SavingsTable';
+import Footer from '@/components/Footer';
+import { useState } from 'react';
+
+export default function Home() {
+	const [calculatedSavings, setCalculatedSavings] = useState(null);
+	const [calculated, toggleCalculated] = useState(false);
+	const [calculating, toggleCalculating] = useState(false);
+
+	return (
+		<main className="bg-light-gray w-full">
+			<div className="w-full h-5 bg-vibrant-green"></div>
+			<Header />
+			<div className="flex flex-col flex-wrap justify-center items-center">
+				<CalculatorForm
+					setCalculatedSavings={setCalculatedSavings}
+					toggleCalculated={toggleCalculated}
+					toggleCalculating={toggleCalculating}
+					calculating={calculating}
+				/>
+				<FAQ />
+				<SavingsTable
+					calculatedSavings={calculatedSavings}
+					calculated={calculated}
+					calculating={calculating}
+				/>
+			</div>
+			<Footer />
+		</main>
+	);
+}
